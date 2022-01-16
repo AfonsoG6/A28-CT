cca1e79d68777eb4103d7d868f7d1695d791d8c4
AfonoG6 <afonsopamg@gmail.com>
2022-01-16 03:25:05 +0000

    Received Msg Table Creation

--- app/src/main/java/com/example/app/database/DatabaseHelper.java
+++ app/src/main/java/com/example/app/database/DatabaseHelper.java
@@ -9,9 +9,6 @@ import androidx.annotation.Nullable;
 import com.example.app.exceptions.NotFoundInDatabaseException;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
-	public static final String TABLE_SKS = "sks";
-	public static final String COLUMN_EPOCH_DAY = "epoch_day";
-	public static final String COLUMN_SK = "sk";
 
 	public DatabaseHelper(@Nullable Context context) {
 		super(context, "sirs.db", null, 1);
@@ -20,20 +17,21 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
 	@Override
 	public void onCreate(SQLiteDatabase sqLiteDatabase) {
-		String stmt = "CREATE TABLE " + TABLE_SKS + " (" + COLUMN_EPOCH_DAY + " INTEGER PRIMARY KEY, " + COLUMN_SK + " BLOB NOT NULL);";
-		// TODO Add Creation of Location Table too
+		String stmt = "CREATE TABLE sks (epoch_day INTEGER PRIMARY KEY NOT NULL, sk BLOB NOT NULL);";
+		stmt += "CREATE TABLE recvd_msgs (interval_n INTEGER NOT NULL, msg BLOB NOT NULL, PRIMARY KEY (interval_n, msg));";
+		//TODO: Add Creation of Location Table too
 		SQLiteDatabase db = this.getWritableDatabase();
 		db.execSQL(stmt);
 	}
 
 	@Override
 	public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-
+		//TODO: Implement this, I have no idea what goes here
 	}
 
 	public void deleteSKsBefore(long epochDay) {
 		SQLiteDatabase db = this.getWritableDatabase();
-		String stmt = "DELETE FROM " + TABLE_SKS + " WHERE " + COLUMN_EPOCH_DAY + " < ?";
+		String stmt = "DELETE FROM sks WHERE epoch_day < ?";
 		String[] args = {String.valueOf(epochDay)};
 		db.execSQL(stmt, args);
 	}
@@ -41,17 +39,17 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 	public boolean insertSK(long epochDay, byte[] sk) {
 		SQLiteDatabase db = this.getWritableDatabase();
 		ContentValues cv = new ContentValues();
-		cv.put(COLUMN_EPOCH_DAY, epochDay);
-		cv.put(COLUMN_SK, sk);
+		cv.put("epoch_day", epochDay);
+		cv.put("sk", sk);
 
-		long insert = db.insert(TABLE_SKS, null, cv);
+		long insert = db.insert("sks", null, cv);
 		return insert >= 0;
 	}
 
 	public byte[] getSK(long epochDay) throws NotFoundInDatabaseException {
 		SQLiteDatabase db = this.getReadableDatabase();
 
-		String stmt = "SELECT " + COLUMN_SK + " FROM " + TABLE_SKS + " WHERE " + COLUMN_EPOCH_DAY + " = ?";
+		String stmt = "SELECT sk FROM sks WHERE epoch_day = ?";
 		String[] args = {String.valueOf(epochDay)};
 		Cursor cursor = db.rawQuery(stmt, args);
 		if (cursor.moveToFirst()) {
