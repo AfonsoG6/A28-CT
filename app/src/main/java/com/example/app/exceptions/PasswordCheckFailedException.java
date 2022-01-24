be9e97858ef0ca3f2f28292a89936446d6da6c18
Afonso Gomes <afonsopamg@gmail.com>
2022-01-24 01:09:16 +0000

    Added Activity and logic for password checking; Added Activity for showing contacts; Implemented getLocationString(lat, long)

---
+++ app/src/main/java/com/example/app/exceptions/PasswordCheckFailedException.java
@@ -0,0 +1,12 @@
+package com.example.app.exceptions;
+
+public class PasswordCheckFailedException extends Exception {
+
+	public PasswordCheckFailedException(String message) {
+		super(message);
+	}
+
+	public PasswordCheckFailedException(Throwable cause) {
+		super(cause);
+	}
+}
