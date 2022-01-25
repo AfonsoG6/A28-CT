f8492c28c3da3d19d4b50b15a900b0a1cf208e92
Mik322 <mchsa@iscte-iul.pt>
2022-01-25 18:28:47 +0000

    ssl script fix

--- postgres/ssl.sh
+++ postgres/ssl.sh
@@ -5,6 +5,9 @@ sudo chmod 400 server.key
 openssl req -new -key server.key -days 3650 -out server.crt -x509 -subj '/C=PT/ST=Lisboa/CN=a28.sirs'
 
 chmod 400 pg_hba.conf
+sudo chown postgres pg_hba.conf postgresql.conf server.key server.crt
 
-sudo mv -t /etc/postgresql/14/main/ pg_hba.conf postgresql.conf server.key server.crt
+sudo cp pg_hba.conf /etc/postgresql/14/main/
+sudo cp postgresql.conf /etc/postgresql/14/main/
+sudo mv -t /etc/postgresql/14/main/ server.key server.crt
 sudo /etc/init.d/postgresql restart
